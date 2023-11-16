5cd83973f2a9a349d7a32eebd114f78f7a61ea8f
thanhcong12345 <150418526+thanhcong12345@users.noreply.github.com>
2023-11-16 21:52:52 +0700

    Update Main C
    Cập nhật bài làm 16/11

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
    printf("Xin chào SRC!")
-   return 0;
-}
-/* Trả lời câu hỏi ở đây:
-
+   ;return 0
+;}
+/*Trả lời câu hỏi ở đây:
+E cảm thấy rất hào hứng và thích thú , cảm thấy bản thân mình phát triển hơn về các kĩ năng mềm và thấy hòa đồng với mọi người hơn !
 
 */
